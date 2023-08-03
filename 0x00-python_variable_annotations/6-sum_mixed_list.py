af1b5ce7bc57ba0fd64e0b6bc9944ae222c5f72e
micoliser <micoliser@gmail.com>
2023-08-03 15:47:52 +0100

    fixing task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,10 +6,7 @@ from typing import List, Union
 def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
     """ sums a list and returns the result """
 
-    return sum(mxd_list)
-
-print(sum_mixed_list.__annotations__)
-mixed = [5, 4, 3.14, 666, 0.99]
-ans = sum_mixed_list(mixed)
-print(ans == sum(mixed))
-print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
+    total = 0
+    for num in mxd_list:
+        total += num
+    return total
