3ccc149151e6ee57228bb843e4ec9a4e16c3ea9c
micoliser <micoliser@gmail.com>
2023-08-03 15:43:28 +0100

    Make files executable

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,3 +7,9 @@ def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
     """ sums a list and returns the result """
 
     return sum(mxd_list)
+
+print(sum_mixed_list.__annotations__)
+mixed = [5, 4, 3.14, 666, 0.99]
+ans = sum_mixed_list(mixed)
+print(ans == sum(mixed))
+print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
