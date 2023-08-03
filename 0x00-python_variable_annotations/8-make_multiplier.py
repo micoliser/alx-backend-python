84d9e2e9b0fb4ed8d62de7230853d16266e504fd
micoliser <micoliser@gmail.com>
2023-08-03 15:29:15 +0100

    Mandatory + advanced done

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" This module contains a type-annotated function make_multiplier """
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ returns a function that multiplies a float by multiplier """
+
+    def mul(f: float) -> float:
+        """ multiplies by multiplier """
+
+        return f * multiplier
+
+    return mul
