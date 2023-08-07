4b9768323f2c64ffe9ac20b4905dcabb0e4eb0d6
micoliser <micoliser@gmail.com>
2023-08-07 18:57:16 +0000

    Fix: typo

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,5 +13,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Awaits for a random delay between 0 and max_delay """
 
-    chilliings = [await wait_random(max_delay) for _ in range(n)]
+    chillings = [await wait_random(max_delay) for _ in range(n)]
     return sorted(chillings)
